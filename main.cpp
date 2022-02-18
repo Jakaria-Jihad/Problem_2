f16bfd70c833c59929b69e40f499e49b9b1b09cc
Jakaria-Jihad <jakariahossen669@gmail.com>
2022-02-18 15:47:54 +0600

    solution added

--- main.cpp
+++ main.cpp
@@ -2,9 +2,22 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
-    int x;
-    cin>>x;
-    cout<<x+1<<'\n';
+    // ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+    string boga, moga;
+    int n, m, result, count = 0;
+    cin >> boga;
+    cin >> moga;
+    n = boga.size();
+    m = moga.size();
+    for(int i = n-1, j = m-1; i>=0, j>=0; i--, j--){
+        if (boga[i]==moga[i])
+        {
+            count++;
+        }else{
+            break;
+        }
+    }
+    result = (n+m)-(2*count);
+    cout << result << "\n";
     return 0;
 }
